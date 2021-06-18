06e178efc7ec96dbea7961e3c654eb6149402261
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-18 14:22:48 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Snack 3
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+// Rivedete l'esercizio js-es6-icons visto in classe!
+
+
+//*Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+
+
+
+
+
+function funcSnack(array,numMin,numMax){
+
+}
