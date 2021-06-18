c9900749b75f08aebc3930f8b391d900d1c5a091
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-18 16:07:49 +0200

    add snack con filter

--- js/script.js
+++ js/script.js
@@ -2,15 +2,23 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
-// Rivedete l'esercizio js-es6-icons visto in classe!
+
 
 
 //*Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 
+//filter
+const numbers = [10,11,12,13,14,15,16,17,18,19];
 
+function newRange(array,numMin,numMax){
 
-
-
-function funcSnack(array,numMin,numMax){
-
+  const range = array.filter((numero,index) => {
+    if(index > numMin && index < numMax){
+      return numero;
+    }
+  });
+  return range;
 }
+
+const range = newRange(numbers,3,6);
+console.log(range);
